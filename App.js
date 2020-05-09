9a1b42fddeb9c5dee077124322b3525966245e86
Logan-Rose <logan.rose0329@gmail.com>
2020-05-09 12:32:48 -0400

    Layout organized, timer start and stop functions completed

--- App.js
+++ App.js
@@ -1,13 +1,68 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
-    </View>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      time: {min:5, sec:20}
+    }
+  }
+  time(){
+    if(this.state.time.sec < 10){
+      return this.state.time.min + ":0" + this.state.time.sec
+    }else{
+      return this.state.time.min + ":" + this.state.time.sec
+    }
+  }
+  
+  decrement(){
+    if(this.state.time.sec === 0){
+      this.setState({time: {
+        min: this.state.time.min - 1, 
+        sec: 59
+      }})
+    } else{
+    this.setState({time: {
+      min: this.state.time.min, 
+      sec: this.state.time.sec -1
+    }})}
+  }
+
+  countDown(){
+    this.timer = setInterval(()=> this.decrement(), 1000 )
+  }
+
+  pause(){
+    clearInterval(this.timer)
+  }
+
+  render(){
+
+    return (
+      <View style={styles.container}>
+        <Text style = {styles.timer}>{this.time()}</Text>
+        <View style={styles.buttons}>
+          <Button onPress= {() => this.countDown()} style = {styles.button} title="Play"/>
+          <Button onPress= {() => this.pause()} style = {styles.button} title="Pause"/>
+          <Button style = {styles.button} title="Reset"/>
+        </View>
+      </View>
+    );
+  }
 }
+
+
+class Timer{
+  constructor(){
+    this.min = 5
+    this.sec = 0
+  }
+
+}
+
+
+
 
 const styles = StyleSheet.create({
   container: {
@@ -16,4 +71,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  timer:{
+    fontSize:40
+  },
+  buttons:{
+    backgroundColor: 'red',
+    flexDirection:'row'
+  },
+  button :{
+
+  }
 });
+
+export default App
